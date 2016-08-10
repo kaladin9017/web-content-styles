d63ea6aa8c577f4c8e33260d87d0383658695604
Jonathan Uy <jon@shinetext.com>
2016-08-10 18:36:41 -0400

    Fixes deploy of js files to correct location on S3

--- deploy.sh
+++ deploy.sh
@@ -30,7 +30,7 @@ node deploy-scripts/compile-advice-home.js
 ## Deploy to S3
 aws s3 cp _tmp/styles.css "s3://$BUCKET/styles.css"
 aws s3 cp _tmp/advice-home.html "s3://$BUCKET/index.html" --content-type="text/html"
-aws s3 cp js/ "s3://$BUCKET/" --recursive
+aws s3 cp js/ "s3://$BUCKET/js/" --recursive
 aws s3 cp templates/article.ejs "s3://$BUCKET/templates/article.ejs" --content-type="text/plain"
 aws s3 cp templates/author.ejs "s3://$BUCKET/templates/author.ejs" --content-type="text/plain"
 

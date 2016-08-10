6fb55e973bc87b9e39d786abfee3da05d222c664
Jonathan Uy <jon@shinetext.com>
2016-08-10 17:49:59 -0400

    Updates deploy process to compile and copy advice home page and js files

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,13 @@ else
   echo "Using bucket: ${BUCKET}"
 fi
 
+## Compile the advice-home template
+node compile-advice-home.js
+
 ## Deploy to S3
 aws s3 cp _tmp/styles.css "s3://$BUCKET/styles.css"
+aws s3 cp _tmp/advice-home.html "s3://$BUCKET/index.html" --content-type="text/html"
+aws s3 cp js/ "s3://$BUCKET/" --recursive
 aws s3 cp templates/article.ejs "s3://$BUCKET/templates/article.ejs" --content-type="text/plain"
 aws s3 cp templates/author.ejs "s3://$BUCKET/templates/author.ejs" --content-type="text/plain"
 
